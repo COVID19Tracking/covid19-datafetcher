aacc10c902d230200c81903cb1e1d718e7f98d47
Alex Johnson <alxmjo@gmail.com>
2020-07-20 10:27:06 -0700

    [MO] races: add positives (#20)
    * [MO] races: add positives

--- fetcher/extras/races.py
+++ fetcher/extras/races.py
@@ -5,3 +5,9 @@ def handle_ak(res, mapping):
     res = res[0].get('features', [])
     collected = {x['attributes']['Race']: x['attributes']['CountCases'] for x in res}
     return map_attributes(collected, mapping, 'AK')
+
+def handle_mo(res, mapping):
+    # Do a quick pass to collect everything then send it to `map_attributes`
+    res = res[0].get('features', [])
+    collected = {x['attributes']['RACE']: x['attributes']['Frequency'] for x in res}
+    return map_attributes(collected, mapping, 'MO')
