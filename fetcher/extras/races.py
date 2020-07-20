be3b4989555f66b5723e0b9561ee015875d313fb
space-buzzer <space_buzzer_dedup@example.com>
2020-07-20 10:35:03 -0700

    Make Flake8 happy and minor refactoring

--- fetcher/extras/races.py
+++ fetcher/extras/races.py
@@ -1,13 +1,18 @@
 from fetcher.utils import map_attributes
 
+
+def collect_attributes(attributes, mapping, field, value, debug_state=None):
+    collected = {x['attributes'][field]: x['attributes'][value] for x in attributes}
+    return map_attributes(collected, mapping, debug_state)
+
+
 def handle_ak(res, mapping):
     # collect values
     res = res[0].get('features', [])
-    collected = {x['attributes']['Race']: x['attributes']['CountCases'] for x in res}
-    return map_attributes(collected, mapping, 'AK')
+    return collect_attributes(res, mapping, 'Race', 'CountCases', 'AK')
+
 
 def handle_mo(res, mapping):
     # Do a quick pass to collect everything then send it to `map_attributes`
     res = res[0].get('features', [])
-    collected = {x['attributes']['RACE']: x['attributes']['Frequency'] for x in res}
-    return map_attributes(collected, mapping, 'MO')
+    return collect_attributes(res, mapping, 'RACE', 'Frequency', 'MO')
