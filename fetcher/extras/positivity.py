31a21ee0a843f87fce5a7619b7cad51f3bf5912d
space-buzzer <space_buzzer_dedup@example.com>
2020-11-19 18:32:20 -0800

    positivity [GA] add daily and alltime components + coercing values to Nan

--- fetcher/extras/positivity.py
+++ fetcher/extras/positivity.py
@@ -17,22 +17,40 @@ def handle_dc(res, mapping):
 
 def handle_ga(res, mapping):
     tagged = []
-    files = ["pcr_positives.csv"]
+    filename = "pcr_positives.csv"
     with zipContextManager(res[-1]) as zipdir:
-        for filename in files:
-            df = pd.read_csv(open(os.path.join(zipdir, filename), 'r'),
-                             parse_dates=['report_date'])
-            df = df[df['county'] == 'Georgia'].filter(mapping.keys())
-            df['UNITS'] = 'Tests'
+        df = pd.read_csv(open(os.path.join(zipdir, filename), 'r'),
+                         parse_dates=['report_date'])
+        df = df[df['county'] == 'Georgia']
 
-            # separate it to 7 & 14 rates
-            windows = {'Week': '7 day percent positive',
-                       '14Days': '14 day percent positive'}
-            for window, column in windows.items():
-                pct = df.rename(columns=mapping).drop(columns='PPR')
-                pct['PPR'] = df[column]
-                pct['WINDOW'] = window
-                tagged.append(pct.to_dict(orient='records'))
+        # alltime/daily
+        latest = df.sort_values('report_date').iloc[-1]
+        # daily
+        tagged.append({
+            'TOTAL': latest['ALL PCR tests performed'],
+            'POSITIVE': latest['All PCR positive tests'],
+            'TIMESTAMP': latest['report_date'],
+            'WINDOW': 'Day',
+            'UNITS': 'Tests'
+        })
+        # all time
+        tagged.append({
+            'TOTAL': latest['Running total of all PCR tests'],
+            'POSITIVE': latest['Running total of all PCR tests.1'],
+            'TIMESTAMP': latest['report_date'],
+            'WINDOW': 'Alltime',
+            'UNITS': 'Tests'
+        })
+
+        # separate it to 7 & 14 rates
+        windows = {'Week': '7 day percent positive',
+                   '14Days': '14 day percent positive'}
+        for window, column in windows.items():
+            pct = df.filter(mapping.keys()).rename(columns=mapping).drop(columns='PPR')
+            pct['PPR'] = pd.to_numeric(df[column], errors='coerce')
+            pct['WINDOW'] = window
+            pct['UNITS'] = 'Tests'
+            tagged.append(pct.to_dict(orient='records'))
 
     return tagged
 
