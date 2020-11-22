6215085c14ad4872b538e032ec1b87311790a0ff
space-buzzer <space_buzzer_dedup@example.com>
2020-11-21 18:17:02 -0800

    positivity [UT][MO] add weekly sources

--- fetcher/extras/positivity.py
+++ fetcher/extras/positivity.py
@@ -68,6 +68,18 @@ def handle_ky(res, mapping):
     return tagged
 
 
+def handle_mo(res, mapping):
+    df = res[0].rename(columns=mapping)
+    df = df[df['County'] == 'All']
+    df = df[['Measure Names', 'PPR', 'TIMESTAMP']]
+    df = df[df['Measure Names'].isin(list(mapping.keys()))]
+    df['WINDOW'] = 'Week'
+    df['UNITS'] = ''
+    df['UNITS'].loc[df['Measure Names'].str.contains('Total Tests')] = 'Tests'
+    df['UNITS'].loc[df['Measure Names'].str.contains('Individual')] = 'People'
+    return df.to_dict(orient='records')
+
+
 def handle_md(res, mapping):
     tagged = []
     df = res[0].rename(columns=mapping)
@@ -80,6 +92,31 @@ def handle_md(res, mapping):
     weekly['PPR'] = df['rolling_avg']
     weekly['WINDOW'] = 'Week'
     tagged.extend(weekly.to_dict(orient='records'))
+    return tagged
+
+
+def handle_ut(res, mapping):
+    tagged = []
+    prefix = "Overview_Total People Tested Seven-Day Rolling Average Percent Positive Rates by Specimen Collection"
+    with zipContextManager(res[-1]) as zipdir:
+        with os.scandir(zipdir) as it:
+            for entry in it:
+                if entry.is_file and entry.name.startswith(prefix):
+                    df = pd.read_csv(
+                        os.path.join(zipdir, entry.name), parse_dates=['Collection Date'])
+                    df = df.rename(columns=mapping)
+                    df['UNITS'] = 'People'
+                    ppr = df[['TIMESTAMP', 'PPR', 'UNITS']]
+                    ppr['WINDOW'] = 'Week'
+                    tagged.extend(ppr.to_dict(orient='records'))
+
+                    # add the daily values
+                    totals = df[['TIMESTAMP', 'UNITS', 'POSITIVE']]
+                    totals['TOTAL'] = df['POSITIVE'] + df['NEGATIVE']
+                    totals['WINDOW'] = 'Day'
+                    tagged.extend(totals.to_dict(orient='records'))
+
+                    break
     return tagged
 
 
