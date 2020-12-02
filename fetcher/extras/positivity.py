e35c8acb0882a1becbe94c0315533cd43b204524
space-buzzer <space_buzzer_dedup@example.com>
2020-12-02 02:29:02 -0800

    positivity: add source-id, fix mo

--- fetcher/extras/positivity.py
+++ fetcher/extras/positivity.py
@@ -12,6 +12,7 @@ def handle_dc(res, mapping):
     ppr = ppr.filter(mapping.keys()).rename(columns=mapping)
     ppr['UNITS'] = 'Tests'
     ppr['WINDOW'] = 'Week'
+    ppr['SID'] = 'dc-1'
     return ppr.to_dict(orient='records')
 
 
@@ -22,6 +23,8 @@ def handle_ga(res, mapping):
         df = pd.read_csv(open(os.path.join(zipdir, filename), 'r'),
                          parse_dates=['report_date'])
         df = df[df['county'] == 'Georgia']
+        sid = 1
+        def get_sid(): "ga-{}".format(sid)
 
         # alltime/daily
         latest = df.sort_values('report_date').iloc[-1]
@@ -31,25 +34,30 @@ def handle_ga(res, mapping):
             'POSITIVE': latest['All PCR positive tests'],
             'TIMESTAMP': latest['report_date'],
             'WINDOW': 'Day',
-            'UNITS': 'Tests'
+            'UNITS': 'Tests',
+            'SID': get_sid(),
         })
         # all time
+        sid += 1
         tagged.append({
             'TOTAL': latest['Running total of all PCR tests'],
             'POSITIVE': latest['Running total of all PCR tests.1'],
             'TIMESTAMP': latest['report_date'],
             'WINDOW': 'Alltime',
-            'UNITS': 'Tests'
+            'UNITS': 'Tests',
+            'SID': get_sid(),
         })
 
         # separate it to 7 & 14 rates
         windows = {'Week': '7 day percent positive',
                    '14Days': '14 day percent positive'}
         for window, column in windows.items():
+            sid += 1
             pct = df.filter(mapping.keys()).rename(columns=mapping).drop(columns='PPR')
             pct['PPR'] = pd.to_numeric(df[column], errors='coerce')
             pct['WINDOW'] = window
             pct['UNITS'] = 'Tests'
+            pct['SID'] = get_sid()
             tagged.append(pct.to_dict(orient='records'))
 
     return tagged
@@ -64,6 +72,7 @@ def handle_ky(res, mapping):
     number = title.find_next_sibling()
     tagged['PPR'] = float(number.get_text(strip=True).replace('%', ''))
     tagged['TIMESTAMP'] = datetime.now().timestamp()
+    tagged['SID'] = 'ky-1'
 
     return tagged
 
@@ -71,12 +80,11 @@ def handle_ky(res, mapping):
 def handle_mo(res, mapping):
     df = res[0].rename(columns=mapping)
     df = df[df['County'] == 'All']
+
     df = df[['Measure Names', 'PPR', 'TIMESTAMP']]
-    df = df[df['Measure Names'].isin(list(mapping.keys()))]
     df['WINDOW'] = 'Week'
-    df['UNITS'] = ''
-    df['UNITS'].loc[df['Measure Names'].str.contains('Total Tests')] = 'Tests'
-    df['UNITS'].loc[df['Measure Names'].str.contains('Individual')] = 'People'
+    df['SID'] = 'mo-3'
+    df = df[df['Measure Names'].isin(list(mapping.keys()))]
     return df.to_dict(orient='records')
 
 
@@ -84,13 +92,14 @@ def handle_md(res, mapping):
     tagged = []
     df = res[0].rename(columns=mapping)
     df['UNITS'] = 'Tests'
-
     df['WINDOW'] = 'Day'
+    df['SID'] = 'md-1'
     tagged.extend(df.to_dict(orient='records'))
 
     weekly = df.drop(columns=['TOTAL', 'POSITIVE', 'PPR'])
     weekly['PPR'] = df['rolling_avg']
     weekly['WINDOW'] = 'Week'
+    weekly['SID'] = 'md-2'
     tagged.extend(weekly.to_dict(orient='records'))
     return tagged
 
@@ -106,14 +115,16 @@ def handle_ut(res, mapping):
                         os.path.join(zipdir, entry.name), parse_dates=['Collection Date'])
                     df = df.rename(columns=mapping)
                     df['UNITS'] = 'People'
-                    ppr = df[['TIMESTAMP', 'PPR', 'UNITS']]
+                    ppr = df.loc[:, ['TIMESTAMP', 'PPR', 'UNITS']]
                     ppr['WINDOW'] = 'Week'
+                    ppr['SID'] = 'ut-1'
                     tagged.extend(ppr.to_dict(orient='records'))
 
                     # add the daily values
-                    totals = df[['TIMESTAMP', 'UNITS', 'POSITIVE']]
+                    totals = df.loc[:, ['TIMESTAMP', 'UNITS', 'POSITIVE']]
                     totals['TOTAL'] = df['POSITIVE'] + df['NEGATIVE']
                     totals['WINDOW'] = 'Day'
+                    totals['SID'] = 'ut-2'
                     tagged.extend(totals.to_dict(orient='records'))
 
                     break
@@ -126,6 +137,7 @@ def handle_va(res, mapping):
     df['PPR'] = pd.to_numeric(df['PPR'].str.rstrip('%'))
     df['WINDOW'] = 'Week'
     df['UNITS'] = 'Tests'
+    df['SID'] = 'va-1'
     return df.to_dict(orient='records')
 
 
@@ -136,6 +148,9 @@ def handle_wa(res, mapping):
     columns = tests.columns
     columns7 = [x for x in columns if x.startswith('7 day rolling')]
     columns1 = [x for x in columns if not x.startswith('7 day rolling')]
+
+    sid = 1
+    def get_sid(): "wa-{}".format(sid)
 
     windows = {'Day': columns1, 'Week': columns7}
     for window, columns in windows.items():
@@ -148,6 +163,8 @@ def handle_wa(res, mapping):
         pct['TOTAL'] = pct['POSITIVE'] + pct['NEGATIVE']
         pct['UNITS'] = 'Tests'
         pct['WINDOW'] = window
+        pct['SID'] = get_sid
+        sid += 1
         tagged.extend(pct.to_dict(orient='records'))
 
     return tagged
@@ -163,6 +180,9 @@ def handle_wi(res, mapping):
     # TODO: example of cheating:
     # could be better to send constants from the query def here
 
+    sid = 1
+    def get_sid(): "wi-{}".format(sid)
+
     for i, df in enumerate(res):
         df.index.name = 'Date'
         df['Date'] = df.index
@@ -170,6 +190,8 @@ def handle_wi(res, mapping):
         df = df.groupby(level=0).last().rename(columns=mapping)
         df['UNITS'] = units[i]
         df['WINDOW'] = 'Week'
+        df['SID'] = get_sid()
+        sid += 1
         mapped.append(df.to_dict(orient='records'))
 
     return mapped
